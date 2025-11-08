96368f3df678e0944a91925524242912805a5ab6
ne0chem <brenkoangelina@gmail.com>
2025-11-08 18:03:19 +0300

    закончила верстку и добавила открытие/закрытие модалки

---
+++ js/my.js
@@ -0,0 +1,28 @@
+const modalWindow = document.querySelector(".modal");
+
+const buttonModals = document.querySelectorAll(".modall__button");
+
+const closeModal = document.querySelector(".modal__close");
+
+const body = document.querySelector("body");
+
+buttonModals.forEach((item) => {
+  item.addEventListener("click", () => {
+    modalWindow.style.display = "flex";
+    body.classList.add("noscroll");
+  });
+});
+
+modalWindow.addEventListener("click", (e) => {
+  const isModal = e.target.closest(".modal__inner");
+
+  if (!isModal) {
+    modalWindow.style.display = "none";
+    body.classList.add("noscroll");
+  }
+});
+
+closeModal.addEventListener("click", () => {
+  modalWindow.style.display = "none";
+  body.classList.add("noscroll");
+});
