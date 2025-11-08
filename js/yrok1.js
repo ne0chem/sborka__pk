002ac3bf63c0345399ea3bcd1ff3504b8b4ea5ac
ne0chem <brenkoangelina@gmail.com>
2025-11-08 19:50:26 +0300

    первый урок по js

---
+++ js/yrok1.js
@@ -0,0 +1,10 @@
+const contents = document.querySelectorAll(".program-line__content");
+
+contents.forEach((elem) => {
+  const title = elem.querySelector(".program-line__title");
+  const descrt = elem.querySelector(".program-line__descr");
+
+  title.addEventListener("click", () => {
+    descrt.classList.toggle("active");
+  });
+});
